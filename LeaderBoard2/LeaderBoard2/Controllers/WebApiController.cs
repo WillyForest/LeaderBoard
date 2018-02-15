c44cadc15df40e5f10378e7b87b4ca48d6e87194
WillyForest <kinashchuksasha@gmail.com>
2018-02-15 13:40:32 +0200

    first commit

---
+++ LeaderBoard2/LeaderBoard2/Controllers/WebApiController.cs
@@ -0,0 +1,36 @@
+﻿using LeaderBoard2.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LeaderBoard2.Controllers
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class WebApiController : ApiController
+    {
+        private LeaderBoardEntities db = new LeaderBoardEntities();
+
+        public ICollection<Player> GetPlayer()
+        {
+            var players = (from Leader in db.Leaders select Leader).ToList();
+
+            Collection<Player> p = new Collection<Player>();
+
+            foreach (Leader l in players)
+            {
+                p.Add(new Player { Id = l.PlayerId, Name = l.Name, Score = l.Score });
+            }
+            return p;
+        }
+    }
+}
---
+++ LeaderBoard2/LeaderBoard2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeaderBoard2.Startup))]
+namespace LeaderBoard2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
